dfecf857ad5231459437c693cf899106e267e67e
IT21338984 <it21338984@my.sliit.lk>
2023-05-16 13:40:17 +0530

    Intial Marketplace Commit

---
+++ backend/models/Advertisement.js
@@ -0,0 +1,52 @@
+ const mongoose = require("mongoose");
+
+ const Schema = mongoose.Schema;
+         
+ const adSchema = new Schema({
+
+    name : {
+         type : String,
+         required: true
+    },
+    category : {
+        type : String,
+        required : true
+    },
+
+    // image : {
+    //     type : String,
+    //     required :true
+    // },
+    price : {
+        type : Number,
+        required : true
+    },
+    weight : {
+        type : Number,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    date: {
+        type: Date,
+        default: () => {
+          const date = new Date();
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, "0");
+          const day = String(date.getDate()).padStart(2, "0");
+          return `${year}-${month}-${day}`;
+        }
+      }
+
+ })
+
+ const Advertisement = mongoose.model("Advertisement",adSchema);
+ 
+
+ module.exports = Advertisement;
